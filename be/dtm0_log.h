76afd58d8e4b1ad4c276eebfca69535d80acb99d
Anilkumar Sindhi <anilkumar.sindhi@seagate.com>
2021-01-18 01:27:24 -0700

    EOS-16458: Updated the usecase when persistent message arrives before txr is logged

--- be/dtm0_log.h
+++ be/dtm0_log.h
@@ -36,8 +36,28 @@
  *  failed participant can be restored by iterating over logged journal to
  *  decide which of the logged messages needs to be sent as a redo to request.
  *
- * define usecases
- * what happens when log_operation appears in the following set states for dtm0 log: S1, S2, Sn?
+ *  During normal operation when every participant and originator is online
+ *  every transaction state is logged and it could be any of the following,
+ *	enum m0_dtm0_log_participant_state {
+ *		M0_DTML_STATE_UNKNOWN,
+ *		M0_DTML_STATE_SENT,
+ *		M0_DTML_STATE_EXECUTED,
+ *		M0_DTML_STATE_PERSISTENT,
+ *	};
+ *
+ *  Every participant maintains the state of it's own transaction which can be
+ *  M0_DTML_STATE_EXECUTED or M0_DTML_STATE_PERSISTENT as well as
+ *  state of other participant which can be either M0_DTML_STATE_UNKNOWN or
+ *  M0_DTML_STATE_PERSISTENT
+ *
+ *  On originator the state of the transaction can be M0_DTML_STATE_UNKNOWN to
+ *  M0_DTML_STATE_PERSISTENT for each participant.
+ *
+ *  During recovery operation of any of the participant rest of the participant
+ *  and originator will iterate over the logged journal and extract the state
+ *  of each transation for participant under recovery from logged information
+ *  and will send redo request for those transaction which  are not
+ *  M0_DTML_STATE_PERSISTENT on participant being recovered.
  *
  * @section Usecases
  *
@@ -52,17 +72,17 @@
  *    struct log*   l;
  *    struct be_tx* tx;
  *    struct be_tx_credit cred;
- *    struct be_dtm0_log *rlist;
  *
  *    log_tx_credit(cred)
  *    tx_open(tx, cred);
  *    if (!log_already_present) {
  *	l = log_create(tx);
+ *      txr = m0_be_dtm0_log_find(l, tx_id);
  *    } else {
- *      l = m0_be_dtm0_log_find(rlist, tx_id);
+ *      txr = m0_be_dtm0_log_find(l, tx_id);
  *    }
  *    ...
- *    log_update(M0_DTML_STATE_EXECUTED, (struct txr*){ ... });
+ *    log_update(l, tx, M0_DTML_STATE_EXECUTED, (struct txr*){ ... });
  *    tx_close(tx);
  * }
  *
@@ -72,27 +92,43 @@
  *    struct log*   l;
  *    struct be_tx* tx;
  *    struct be_tx_credit cred;
- *    struct be_dtm0_log *rlist;
  *
  *    log_tx_credit(cred)
  *    tx_open(tx, cred);
  *
- *    l = m0_be_dtm0_log_find(rlist, tx_id);
- *    M0_ASSERT(l != NULL);
+ *    txr = m0_be_dtm0_log_find(l, tx_id);
+ *
  *    ...
- *    log_update(M0_DTML_STATE_PERSISTENT, (struct txr*){ ... });
+ *    log_update(l, tx, M0_DTML_STATE_PERSISTENT, (struct txr*){ ... });
  *    tx_close(tx);
  * }
  *
-// 1. usecase when PRESISTENT message arrives before EXECUTED
-// 2.
-// 3.
+ * 3. When transaction become persistent on other participant, corresponding
+ *    state of participant in journal log will be updated.
+ *    If log is not present the it will create the log and update the state,
+ *    rest of the informatin will be invalid
+ *    TODO: What about txr payload it will also be invalid, as persistent
+ *    message do not contains the txr payload?
+ *
+ * {
+ *    struct log*   l;
+ *    struct be_tx* tx;
+ *    struct be_tx_credit cred;
+ *
+ *    log_tx_credit(cred)
+ *    tx_open(tx, cred);
+ *
+ *    if (!log_already_present) {
+ *	l = log_create(tx);
+ *      txr = m0_be_dtm0_log_find(l, tx_id);
+ *    } else {
+ *      txr = m0_be_dtm0_log_find(l, tx_id);
+ *    }
+ *
+ *    log_update(l, tx, M0_DTML_STATE_PERSISTENT, (struct txr*){ ... });
+ *    tx_close(tx);
+ * }
  */
-// define the scope of dtm0 log
-// persistent and volatile structure
-// the goal of the structure (what is it for?)
-// implementation details (usecases)
-// dependencies on the other modules
 
 /* Participant state */
 enum m0_dtm0_log_participant_state {
@@ -107,12 +143,13 @@ enum m0_be_dtm0_log_op {
 	M0_DTML_ON_SENT,
 	M0_DTML_ON_EXECUTED,
 	M0_DTML_ON_PERSISTENT,
-	M0_DTML_ON_REDO
+	M0_DTML_ON_REDO /* TODO: I don't think so it is needed */
 };
 
 struct m0_be_dtm0_log {
-	struct m0_mutex   lock; /* volatile structure */
-	struct m0_be_list list; /* persistent structure */
+	struct m0_mutex   lock;  /* volatile structure */
+	struct m0_be_list list;  /* persistent structure */
+	struct m0_list    vlist; /* Volatile list */
 };
 
 /* Unique identifier for request */
@@ -126,18 +163,15 @@ struct m0_dtm0_log_participant_list {
 	enum m0_dtm0_log_participant_state pstate;
 };
 
-
 // define in a separate header?
 struct m0_dtm0_txr {
 	struct m0_dtm0_dtx_id                tid;
+	/*TODO: Should this be bufvec or something or static is fine? */
 	struct m0_dtm0_log_participant_list  plist[M0_DTM0_MAX_PARTICIPANT];
 	struct m0_buf                        txr_payload;
-	// participants
-	// states of participants
-	// dtx id
-	// version???
 };
 
+/* DTM0 log specific wraper for txr */
 struct m0_dtm0_log_record {
 	struct m0_dtm0_txr txr;
 };
@@ -155,8 +189,6 @@ M0_INTERNAL struct m0_be_dtm0_log * m0_be_dtm0_log_create(struct m0_be_tx *tx);
 
 M0_INTERNAL void m0_be_dtm0_log_destroy(struct m0_be_dtm0_log **log,
 					struct m0_be_tx        *tx);
-
-
 
 // operational interfaces
 M0_INTERNAL void m0_be_dtm0_log_update(struct m0_be_dtm0_log     *log,
